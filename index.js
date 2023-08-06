27d282277bae17fe9a1fc795bf8e6eee5ff06c16
Kosisochukwu Obidiegwu <105119703+lotathebaby@users.noreply.github.com>
2023-08-05 23:55:35 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const openNav = document.querySelector('.nav-open');
+const closeNav = document.querySelector('.nav-close');
+const mobileNav = document.querySelector('.mobile-nav');
+
+openNav.addEventListener('click', () => {
+    mobileNav.style.display = "block"
+    closeNav.style.display = "block"
+    openNav.style.display = "none"
+})
+
+console.log(openNav)
+
+closeNav.addEventListener('click', () => {
+    mobileNav.style.display = "none"
+    closeNav.style.display = "none"
+    openNav.style.display = "block"
+})
+
+
+const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+
+const numBalls = 50;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 30)}em`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  document.body.append(ball);
+}
