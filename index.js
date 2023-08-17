d778e9718431942432a632eae8ceae6a171bbcf5
greatadams <75841998+greatadams@users.noreply.github.com>
2023-08-16 22:23:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,16 @@
-const openNav = document.querySelector('.nav-open');
-const closeNav = document.querySelector('.nav-close');
-const mobileNav = document.querySelector('nav');
-const searchBox = document.querySelector('.searchBox')
 
-openNav.addEventListener('click', () => {
-    mobileNav.style.display = "block"
-    closeNav.style.display = "block"
-    openNav.style.display = "none"
-    searchBox.style.marginTop = "4em"
-})
+const primaryNav=document.querySelector(".primary-navigation")
+const navToggle=document.querySelector(".mobile-nav-toggle")
 
-console.log(openNav)
+navToggle.addEventListener('click',() => {
+    const visibility =primaryNav.getAttribute('data-visible')
+console.log(visibility)
 
-closeNav.addEventListener('click', () => {
-    mobileNav.style.display = "none"
-    closeNav.style.display = "none"
-    openNav.style.display = "block"
-    searchBox.style.marginTop = "-8em"
-})
-
-
-
-const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
-
-const numBalls = 50;
-const balls = [];
-
-for (let i = 0; i < numBalls; i++) {
-  let ball = document.createElement("div");
-  ball.classList.add("ball");
-  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-  ball.style.top = `${Math.floor(Math.random() * 30)}em`;
-  ball.style.transform = `scale(${Math.random()})`;
-  ball.style.width = `${Math.random()}em`;
-  ball.style.height = ball.style.width;
-  
-  balls.push(ball);
-  document.body.append(ball);
+if(visibility === 'false'){
+    primaryNav.setAttribute('data-visible',true);
+    navToggle.setAttribute('aria-expanded',true);
+}else if(visibility === 'true'){
+    primaryNav.setAttribute('data-visible',false);
+    navToggle.setAttribute('aria-expanded',false);
 }
+})
